bd6c844aa77cef7ec7a5c318c55583f4c1830759
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-13 15:39:24 +0900

    docs(class): update example

--- class.ts
+++ class.ts
@@ -7,9 +7,18 @@ import { Emplaceable } from "./mixin.ts";
  *
  * @example
  * ```ts
- * import { EmplaceableMap } from "https://deno.land/x/upsert@$VERSION/class.ts";
+ * import { EmplaceableMap } from "https://deno.land/x/upsert@$VERSION/mod.ts";
+ * import { assert } from "https://deno.land/std/testing/asserts.ts";
  *
- * const map = new EmplaceableMap();
+ * const map = new EmplaceableMap<string, number>();
+ * declare const key: string;
+ *
+ * map.emplace(key, {
+ *  insert: () => 0,
+ *  update: (existing) => existing + 1,
+ * });
+ *
+ * assert(map.has(key));
  * ```
  */
 export class EmplaceableMap<K, V>
