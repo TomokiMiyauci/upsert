3cb5326f621cca3d74b35cb6cb0fcd664343479b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-13 14:38:40 +0900

    feat(mod): export related modules

---
+++ mod.ts
@@ -0,0 +1,5 @@
+// Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export { Emplaceable, EmplaceableMap, EmplaceableWeakMap } from "./mixin.ts";
+export { emplace, type EmplaceHandler, type MapLike } from "./emplace.ts";
