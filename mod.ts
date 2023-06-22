602ecb7fbd254d5f317becf037003757810cffe2
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-22 21:10:09 +0900

    docs(mod): add description

--- mod.ts
+++ mod.ts
@@ -1,6 +1,10 @@
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
+/** Maps for emplace, TC39 [proposal-upsert](https://github.com/tc39/proposal-upsert) implementation.
+ * @module
+ */
+
 export { Emplaceable, emplaceable } from "./mixin.ts";
 export {
   emplace,
