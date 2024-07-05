ad02c2bd2b1ae6122e7a596458a3acfe464a0f26
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-07-05 15:20:12 +0900

    fix(class): change superclass extension into a variable
    to avoid jsr restriction

--- src/class.ts
+++ src/class.ts
@@ -1,7 +1,21 @@
+// deno-lint-ignore-file no-explicit-any
 // Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
 
 import { Emplaceable } from "./mixin.ts";
+
+interface EmplaceableMapConstructor {
+  new (): Map<any, any> & Emplaceable<any, any>;
+  new <K, V>(
+    entries?: readonly (readonly [K, V])[] | null,
+  ): Map<K, V> & Emplaceable<K, V>;
+
+  readonly prototype: Map<any, any> & Emplaceable<any, any>;
+}
+
+const _EmplaceableMap: EmplaceableMapConstructor = /* @__PURE__ */ Emplaceable(
+  Map,
+);
 
 /** {@link Map} with {@link Emplaceable} implementation.
  *
@@ -19,8 +33,18 @@ import { Emplaceable } from "./mixin.ts";
  * assert(map.emplace);
  * ```
  */
-export class EmplaceableMap<K, V>
-  extends /* @__PURE__ */ Emplaceable(Map)<K, V> {}
+export class EmplaceableMap<K, V> extends _EmplaceableMap<K, V> {}
+
+interface EmplaceableWeakMapConstructor {
+  new <K extends WeakKey = WeakKey, V = any>(
+    entries?: readonly (readonly [K, V])[] | null,
+  ): WeakMap<K, V> & Emplaceable<K, V>;
+
+  readonly prototype: WeakMap<WeakKey, any> & Emplaceable<any, any>;
+}
+
+const _EmplaceableWeakMap: EmplaceableWeakMapConstructor =
+  /* @__PURE__ */ Emplaceable(WeakMap);
 
 /** {@link WeakMap} with {@link Emplaceable} implementation.
  *
@@ -38,5 +62,5 @@ export class EmplaceableMap<K, V>
  * assert(weakMap.emplace);
  * ```
  */
-export class EmplaceableWeakMap<K extends object, V>
-  extends /* @__PURE__ */ Emplaceable(WeakMap)<K, V> {}
+export class EmplaceableWeakMap<K extends WeakKey, V>
+  extends _EmplaceableWeakMap<K, V> {}
